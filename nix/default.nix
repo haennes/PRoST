c8b823177de3da1a395e8687541b6ecda9ddaf7a
haennes <hannes.hofmuth@gmail.com>
2025-01-14 19:24:30 +0100

    wip

--- nix/default.nix
+++ nix/default.nix
@@ -2,7 +2,7 @@ self:
 { lib, config, ... }:
 let cfg = config.services.prost;
 in {
-  imports = [ (import ./docker-compose.nix self) ./options.nix ];
+  imports = [ (import ./docker-compose.nix self) (import ./options.nix self) ];
   config = lib.mkIf cfg.enable {
     services.nginx = {
       enable = true;
